cdead43478347529c7178d8c03d3232845cd071a
OtakuBoy420 <U28282@rts-365.dk>
2023-03-14 11:58:39 +0100

    add image placeholder and remove console log

--- src/components/subcomponents/ActivityCard.jsx
+++ src/components/subcomponents/ActivityCard.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import ImagePlaceholder from "../global/ImagePlaceholder"
 export default function ActivityCard({ activity }) {
+  const [imageLoaded, setImageLoaded] = useState(false)
+  const handleImageLoad = () => {
+    setImageLoaded(true)
+  }
   return (
     <article className="relative flex aspect-square w-full cursor-pointer flex-col rounded-[39px] rounded-br-none">
       <Link className="h-full w-full rounded-[39px] rounded-br-none" to={`/activity/${activity?.id}`}>
-        <img src={activity?.asset.url} alt={activity?.name} className="-z-1 absolute inset-0 h-full w-full rounded-[39px] rounded-br-none object-cover" />
+        {!imageLoaded && <ImagePlaceholder size="full" />}
+        <img
+          onLoad={handleImageLoad}
+          className={imageLoaded ? "-z-1 absolute inset-0 h-full w-full rounded-[39px] rounded-br-none object-cover" : "hidden"}
+          src={activity?.asset.url}
+          alt={activity?.name}
+        />
+        <img />
         <div className="absolute bottom-0 flex h-[30%] w-full flex-col justify-center rounded-tr-[39px] rounded-bl-[39px] bg-secondary/80 px-6 text-black">
           <h2>{activity?.name}</h2>
           <p>
