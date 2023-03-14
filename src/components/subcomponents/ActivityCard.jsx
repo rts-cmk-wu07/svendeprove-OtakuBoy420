3ea5d830ea9e6909c8825a03cfa0453aaa6bf6c3
OtakuBoy420 <U28282@rts-365.dk>
2023-03-14 11:24:51 +0100

    activity details page done

--- src/components/subcomponents/ActivityCard.jsx
+++ src/components/subcomponents/ActivityCard.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 export default function ActivityCard({ activity }) {
   return (
     <article className="relative flex aspect-square w-full cursor-pointer flex-col rounded-[39px] rounded-br-none">
-      <Link className="h-full w-full rounded-[39px] rounded-br-none" to={`/activity/${activity.id}`}>
-        <img src={activity.asset.url} alt={activity.name} className="-z-1 absolute inset-0 h-full w-full rounded-[39px] rounded-br-none object-cover" />
+      <Link className="h-full w-full rounded-[39px] rounded-br-none" to={`/activity/${activity?.id}`}>
+        <img src={activity?.asset.url} alt={activity?.name} className="-z-1 absolute inset-0 h-full w-full rounded-[39px] rounded-br-none object-cover" />
         <div className="absolute bottom-0 flex h-[30%] w-full flex-col justify-center rounded-tr-[39px] rounded-bl-[39px] bg-secondary/80 px-6 text-black">
-          <h2>{activity.name}</h2>
+          <h2>{activity?.name}</h2>
           <p>
-            {activity.minAge}-{activity.maxAge} år
+            {activity?.minAge}-{activity?.maxAge} år
           </p>
         </div>
       </Link>
